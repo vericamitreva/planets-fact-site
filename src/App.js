4226ae5c13f2c36af180fc79feabbf9156c72e88
vericamitreva <mitrevav98@gmail.com>
2024-05-10 21:18:54 +0200

    Small Update

--- src/App.js
+++ src/App.js
@@ -5,13 +5,6 @@ function App() {
   return (
     <>
       <Home />
-      {/* <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/destination" element={<Destination />} />
-        <Route path="/crew" element={<Crew />} /> 
-        <Route path="/technology" element={<Technology />} />
-      </Routes> */}
     </>
   );
 }
